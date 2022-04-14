d5a280cb06e1c72e8ac21d73c2e47a0502f6c12b
LPF9000 <56581520+LPF9000@users.noreply.github.com>
2022-04-13 23:36:40 -0400

    test

--- lab2/fib.sv
+++ lab2/fib.sv
@@ -25,98 +25,96 @@
         until go is asserted again, and then is cleared on the next cycle.
  ============================================================================ */
 
-module fib_bad
-  #(
+module fib_bad #(
     parameter int INPUT_WIDTH,
     parameter int OUTPUT_WIDTH
-    )
-   (
-    input logic 		    clk,
-    input logic 		    rst,
-    input logic 		    go,
-    input logic [INPUT_WIDTH-1:0]   n, 
+) (
+    input  logic                    clk,
+    input  logic                    rst,
+    input  logic                    go,
+    input  logic [ INPUT_WIDTH-1:0] n,
     output logic [OUTPUT_WIDTH-1:0] result,
-    output logic 		    overflow,
-    output logic 		    done
-   );
+    output logic                    overflow,
+    output logic                    done
+);
 
-   typedef enum {START, COND, COMPUTE, OVERFLOW, DONE, RESTART} state_t;
-   state_t state_r;   
-   
-   logic [$bits(n)-1:0] i_r;
-   logic [$bits(result)-1:0] x_r;
-   logic [$bits(result)-1:0] y_r;
-   logic [$bits(result):0] full_add_r;   
+  typedef enum {
+    START,
+    COND,
+    COMPUTE,
+    OVERFLOW,
+    DONE,
+    RESTART
+  } state_t;
+  state_t state_r;
 
-   logic [$bits(result)-1:0] result_r;   
-   logic 		     done_r, overflow_r;
+  logic [$bits(n)-1:0] i_r;
+  logic [$bits(result)-1:0] x_r;
+  logic [$bits(result)-1:0] y_r;
+  logic [$bits(result):0] full_add_r;
 
-   assign done = done_r;
-   assign result = result_r;
-   assign overflow = overflow_r;
-         
-   always_ff @(posedge clk or posedge rst) begin
-      if (rst) begin
-	 state_r <= START;
-	 result_r <= '0;
-	 done_r <= 1'b0;
-	 i_r <= '0;
-	 x_r <= '0;
-	 y_r <= '0;
-	 overflow_r <= 1'b0;
-	 full_add_r <= '0;	 
-      end
-      else begin
-       
-	 case (state_r)
-	   START : begin
-	      i_r <= 3;
-	      x_r <= '0;
-	      y_r <= 1;	   	      
-	      if (go == 1'b1) state_r <= COND;	      
-	   end
-	   
-	   COND : begin
-	      done_r <= 1'b0;
-	      
-	      if (i_r <= n)
-		state_r <= COMPUTE;
-	      else
-		state_r <= DONE;	   
-	   end
-	   
-	   COMPUTE : begin	      
-	      x_r <= y_r;
-	      full_add_r <= x_r + y_r;
-	      i_r <= i_r + 1'b1;
-	      state_r <= OVERFLOW;	      
-	   end
+  logic [$bits(result)-1:0] result_r;
+  logic done_r, overflow_r;
 
-	   OVERFLOW : begin
-	      if (full_add_r[OUTPUT_WIDTH]) overflow_r <= 1'b1;
-	      y_r <= full_add_r;	      
-	      state_r <= COND;						
-	   end-
+  assign done = done_r;
+  assign result = result_r;
+  assign overflow = overflow_r;
 
-	   DONE : begin
-	      if (n < 2)
-		result_r <= x_r;
-	      else
-		result_r <= y_r;
-	      
-	      done_r <= 1'b1;	      
-	      state_r <= RESTART;	      	     
-	   end
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      state_r <= START;
+      result_r <= '0;
+      done_r <= 1'b0;
+      i_r <= '0;
+      x_r <= '0;
+      y_r <= '0;
+      overflow_r <= 1'b0;
+      full_add_r <= '0;
+    end else begin
 
-	   RESTART : begin
-	      if (go == 1'b1) state_r <= COND; 
-	   end
-	 endcase
+      case (state_r)
+        START: begin
+          i_r <= 3;
+          x_r <= '0;
+          y_r <= 1;
+          if (go == 1'b1) state_r <= COND;
+        end
 
-	 if (go == 1'b1)
-	   state_r <= COND;	      	
-      end
-   end   
+        COND: begin
+          done_r <= 1'b0;
+
+          if (i_r <= n) state_r <= COMPUTE;
+          else state_r <= DONE;
+        end
+
+        COMPUTE: begin
+          x_r <= y_r;
+          full_add_r <= x_r + y_r;
+          i_r <= i_r + 1'b1;
+          state_r <= OVERFLOW;
+        end
+
+        OVERFLOW: begin
+          if (full_add_r[OUTPUT_WIDTH]) overflow_r <= 1'b1;
+          y_r <= full_add_r;
+          state_r <= COND;
+        end
+        DONE: begin
+          if (n < 2) result_r <= x_r;
+          else result_r <= y_r;
+
+          done_r  <= 1'b1;
+          state_r <= RESTART;
+        end
+
+        RESTART: begin
+          if (go == 1'b1) state_r <= COND;
+        end
+      endcase
+
+      if (go == 1'b1) state_r <= COND;
+    end
+  end
 endmodule
 
 
@@ -126,44 +124,45 @@ endmodule
 // very thorough testbench, so if your testbench is lacking, I will likely
 // will find errors you did not see in your tests.
 
-module fib_good
-  #(
+module fib_good #(
     parameter int INPUT_WIDTH,
     parameter int OUTPUT_WIDTH
-    )
-   (
-    input logic 		    clk,
-    input logic 		    rst,
-    input logic 		    go,
-    input logic [INPUT_WIDTH-1:0]   n, 
+) (
+    input  logic                    clk,
+    input  logic                    rst,
+    input  logic                    go,
+    input  logic [ INPUT_WIDTH-1:0] n,
     output logic [OUTPUT_WIDTH-1:0] result,
-    output logic 		    overflow,
-    output logic 		    done
-   );
+    output logic                    overflow,
+    output logic                    done
+);
 
-   
+
 endmodule
 
 
 // Top-level module for synthesis and simulation, change the instantiated
 // module to test different modules.
 
-module fib
-  #(
-    parameter int INPUT_WIDTH=6,
-    parameter int OUTPUT_WIDTH=32
-    )
-   (
-    input logic 		    clk,
-    input logic 		    rst,
-    input logic 		    go,
-    input logic [INPUT_WIDTH-1:0]   n, 
+module fib #(
+    parameter int INPUT_WIDTH  = 6,
+    parameter int OUTPUT_WIDTH = 32
+) (
+    input  logic                    clk,
+    input  logic                    rst,
+    input  logic                    go,
+    input  logic [ INPUT_WIDTH-1:0] n,
     output logic [OUTPUT_WIDTH-1:0] result,
-    output logic 		    overflow,
-    output logic 		    done
-   );
+    output logic                    overflow,
+    output logic                    done
+);
 
-   fib_bad #(.INPUT_WIDTH(INPUT_WIDTH), .OUTPUT_WIDTH(OUTPUT_WIDTH)) top (.*);
-   //fib_good #(.INPUT_WIDTH(INPUT_WIDTH), .OUTPUT_WIDTH(OUTPUT_WIDTH)) top (.*);
-   
+  fib_bad #(
+      .INPUT_WIDTH (INPUT_WIDTH),
+      .OUTPUT_WIDTH(OUTPUT_WIDTH)
+  ) top (
+      .*
+  );
+  //fib_good #(.INPUT_WIDTH(INPUT_WIDTH), .OUTPUT_WIDTH(OUTPUT_WIDTH)) top (.*);
+
 endmodule
