0c8875638404059a0a8d33042af586cde6c8bb94
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2022-04-14 21:10:43 +0000

    covers design spec bullet point 2

--- lab2/fib_tb.sv
+++ lab2/fib_tb.sv
@@ -80,6 +80,10 @@ module fib_tb;
   assert property (@(posedge bfm.clk) disable iff (bfm.rst) bfm.go && bfm.done |=> !bfm.done)
   else $error("Time %0t [Assert Property]: Done=1, go=1, done not cleared next cycle.", $time);
 
+  // if done is asserted, but go is not asserted, done should remain true.
+  assert property (@(posedge bfm.clk) disable iff (bfm.rst) bfm.done && !bfm.go |-> $stable(bfm.done)) 
+  else $error("Time %0t [Assert Property]: Done=1, go=1, done not stable.", $time);
+
   // if done is cleared, then go should have been asserted on the previous clock cycle
   assert property (@(posedge bfm.clk) disable iff (bfm.rst) $fell(bfm.done) |-> $past(bfm.go, 1))
   else $error("Time %0t [Assert Property]: done not cleared after go asserted.", $time);
