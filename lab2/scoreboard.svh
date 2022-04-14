04a2536da0e36206904c1667f1dab6b9a363b4aa
LPF9000 <56581520+LPF9000@users.noreply.github.com>
2022-04-14 13:07:08 -0400

    test

--- lab2/scoreboard.svh
+++ lab2/scoreboard.svh
@@ -13,15 +13,17 @@ class scoreboard #(
   mailbox scoreboard_result_mailbox;
   mailbox scoreboard_n_mailbox;
   mailbox scoreboard_overflow_mailbox;
-  int     passed,                      failed, reference1, reference2;
+  int     result_passed, result_failed, overflow_passed, overflow_failed, reference1, reference2;
 
   function new(mailbox scoreboard_n_mailbox, mailbox scoreboard_result_mailbox);
     this.scoreboard_n_mailbox = scoreboard_n_mailbox;
     this.scoreboard_result_mailbox = scoreboard_result_mailbox;
     //this.scoreboard_overflow_mailbox = scoreboard_overflow_mailbox;
 
-    passed = 0;
-    failed = 0;
+    result_passed = 0;
+    result_failed = 0;
+    overflow_passed = 0;
+    overflow_failed = 0;
   endfunction  // new
 
   // Reference model for the correct result.
@@ -74,23 +76,23 @@ class scoreboard #(
       // Get the correct result based on the input at the start of the test.
       reference1 = result_model(in_item.n);
       if (out_item.result == reference1) begin
-        $display("Time %0t [Scoreboard] Test passed for n=h%h", $time, in_item.n);
-        passed++;
+        $display("Time %0t [Scoreboard] Result test passed for n=h%h", $time, in_item.n);
+        result_passed++;
       end else begin
-        $display("Time %0t [Scoredboard] Test failed: result = %0d instead of %0d for n = h%h.",
+        $display("Time %0t [Scoredboard] Result test failed: result = %0d instead of %0d for n = h%h.",
                  $time, out_item.result, reference1, in_item.n);
-        failed++;
+        result_failed++;
       end
 
       // Get the correct overflow based on the input at the start of the test.
       reference2 = overflow_model(in_item.n);
       if (out_item.overflow == reference2) begin
-        $display("Time %0t [Scoreboard] Test passed for n=h%h", $time, in_item.n);
-        passed++;
+        $display("Time %0t [Scoreboard] Overflow test passed for n=h%h", $time, in_item.n);
+        overflow_passed++;
       end else begin
-        $display("Time %0t [Scoredboard] Test failed: overflow = %0d instead of %0d for n = h%h.",
+        $display("Time %0t [Scoredboard] Overflow test failed: overflow = %0d instead of %0d for n = h%h.",
                  $time, out_item.overflow, reference2, in_item.n);
-        failed++;
+        overflow_failed++;
       end
     end  // for (int i=0; i < num_tests; i++)
 
@@ -104,7 +106,8 @@ class scoreboard #(
   endtask
 
   function void report_status();
-    $display("Test status: %0d passed, %0d failed", passed, failed);
+    $display("Test status: %0d result_passed, %0d result_failed", result_passed, result_failed);
+    $display("Test status: %0d overflow_passed, %0d overflow_failed", overflow_passed, overflow_failed);
   endfunction
 
 endclass
