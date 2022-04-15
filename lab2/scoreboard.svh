719e53094bbcc0f59d6f6da3e38bb92c42653faf
LPF9000 <56581520+LPF9000@users.noreply.github.com>
2022-04-14 20:39:34 -0400

    test

--- lab2/scoreboard.svh
+++ lab2/scoreboard.svh
@@ -62,7 +62,7 @@ class scoreboard #(
 
       // First wait until the driver informs us of a new test.
       scoreboard_n_mailbox.get(in_item);
-      $display("Time %0t [Scoreboard]: Received start of test for n=h%h and i_r = h%h.", $time,
+      $display("Time %0t [Scoreboard]: Received start of test for n=h%h", $time,
                in_item.n);
 
       // Then, wait until the monitor tells us that test is complete.
