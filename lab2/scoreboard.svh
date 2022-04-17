800bd6817e1788b169cc02f4f454677deac92991
LPF9000 <56581520+LPF9000@users.noreply.github.com>
2022-04-16 22:15:30 -0400

    test

--- lab2/scoreboard.svh
+++ lab2/scoreboard.svh
@@ -188,9 +188,6 @@ class scoreboard #(
         y_r_failed++;
       end
 
-      //full_add_r = full_add_r[OUTPUT_WIDTH-1:0];
-      // if (out_item.full_add_r[OUTPUT_WIDTH] == 0 && out_item.overflow == 1)
-      // temp = longint'({1'b1,temp[OUTPUT_WIDTH-1:0]});
       temp = int'(full_add_r[OUTPUT_WIDTH-1:0]);
       if (out_item.full_add_r[OUTPUT_WIDTH-1:0] == temp) begin
         $display("Time %0t [Scoreboard] Result test passed for full_add_r=h%h", $time, temp);
