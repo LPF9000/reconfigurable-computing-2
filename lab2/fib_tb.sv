09697e5fa88874db4f3021b4d0ef8943696bd123
LPF9000 <56581520+LPF9000@users.noreply.github.com>
2022-04-17 18:56:07 -0400

    test

--- lab2/fib_tb.sv
+++ lab2/fib_tb.sv
@@ -123,6 +123,9 @@ module fib_tb;
   assert property (@(posedge bfm.clk) disable iff (bfm.rst) bfm.done && $stable(bfm.done) |-> $stable(bfm.overflow))
   else $error("Time %0t [Assert Property]: Done=1, overflow not stable.", $time);
 
+  assert property (@(posedge bfm.clk) disable iff (bfm.rst) !bfm.done |=> $rose(bfm.done) within ($rose(bfm.clk) ##[0:100000000] $fell(bfm.clk)))
+  else $error("Time %0t [Assert Property]: Done=1, overflow not stable.", $time);
+
   // Check overflow asserted
   // assert property (@(posedge clk) disable iff (bfm.rst) )
   // else $error("Time %0t [Assert Property]: ", $time);
