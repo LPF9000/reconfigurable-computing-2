903d4aa28505a513d5fad0947aa8c13ab2bdb1ba
LPF9000 <56581520+LPF9000@users.noreply.github.com>
2022-04-16 22:52:02 -0400

    adding more tests

--- lab2/fib_tb.sv
+++ lab2/fib_tb.sv
@@ -5,8 +5,8 @@
 
 module fib_tb;
 
-  localparam int NUM_RANDOM_TESTS = 1000;
-  localparam int NUM_CONSECUTIVE_TESTS = 200;
+  localparam int NUM_RANDOM_TESTS = 5000;
+  localparam int NUM_CONSECUTIVE_TESTS = 1000;
   localparam int NUM_REPEATS = 4;
   localparam int INPUT_WIDTH = 6;
   localparam int OUTPUT_WIDTH = 32;
